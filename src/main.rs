d8c028180a142d07c3bd2d6fb30f4840d12bf888
crumblingstatue <radiantstatue@gmail.com>
2022-06-22 12:21:59 +0200

    Improve FfmpegTimeFmt and add tests

--- src/main.rs
+++ src/main.rs
@@ -157,19 +157,25 @@ struct FfmpegTimeFmt(f64);
 
 impl fmt::Display for FfmpegTimeFmt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        const HOUR: f64 = 3600.0;
-        const MINUTE: f64 = 60.0;
-
         let secs = self.0;
-        let hh = secs / HOUR;
-        let mm = secs / MINUTE;
+        let hh = secs / 3600.0;
+        let mm = hh.fract() * 60.0;
+        let ss = mm.fract() * 60.0;
         write!(
             f,
             "{:02.0}:{:02.0}:{:02.0}.{:03}",
-            hh,
-            mm,
-            secs % MINUTE,
-            (secs.fract() * 1000.0) as u64
+            hh.floor(),
+            mm.floor(),
+            ss.floor(),
+            (ss.fract() * 1000.0).round() as u64
         )
     }
+}
+
+#[test]
+fn test_time_fmt() {
+    assert_eq!(&FfmpegTimeFmt(0.0).to_string()[..], "00:00:00.000");
+    assert_eq!(&FfmpegTimeFmt(24.56).to_string()[..], "00:00:24.560");
+    assert_eq!(&FfmpegTimeFmt(119.885).to_string()[..], "00:01:59.885");
+    assert_eq!(&FfmpegTimeFmt(52349.345).to_string()[..], "14:32:29.345");
 }
