7ca792d447abe1697400a22f3481d6e596da6b4d
crumblingstatue <radiantstatue@gmail.com>
2022-06-23 20:11:09 +0200

    Move bottom bar ui to its own function

--- src/ui.rs
+++ src/ui.rs
@@ -50,70 +50,7 @@ pub(crate) fn ui(
 ) {
     {
         let re = egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label(format!(
-                    "{}/{}",
-                    FfmpegTimeFmt(src_info.time_pos),
-                    FfmpegTimeFmt(src_info.duration)
-                ));
-                ui.style_mut().spacing.slider_width = ui.available_width();
-                let mut pos = src_info.time_pos;
-                if ui
-                    .add(egui::Slider::new(&mut pos, 0.0..=src_info.duration).show_value(false))
-                    .changed()
-                {
-                    mpv.set_property::<TimePos>(pos);
-                }
-            });
-            ui.horizontal(|ui| {
-                let mut changed = false;
-                ui.label("Video width");
-                if ui.add(egui::DragValue::new(&mut present.dim.x)).changed() {
-                    present.dim.y = (present.dim.x as f64 / src_info.w_h_ratio) as VideoMag;
-                    changed = true;
-                }
-                ui.label("Video height");
-                if ui.add(egui::DragValue::new(&mut present.dim.y)).changed() {
-                    present.dim.x = (present.dim.y as f64 * src_info.w_h_ratio) as VideoMag;
-                    changed = true;
-                }
-                if ui.button("orig").clicked() {
-                    present.dim.x = src_info.dim.x as VideoMag;
-                    present.dim.y = src_info.dim.y as VideoMag;
-                    changed = true;
-                }
-                if ui.button("fit").clicked() {
-                    present.dim.y = video_area_max_dim.y;
-                    present.dim.x = (present.dim.y as f64 * src_info.w_h_ratio) as VideoMag;
-                    if present.dim.x > video_area_max_dim.x {
-                        present.dim.x = video_area_max_dim.x;
-                        present.dim.y = (present.dim.x as f64 / src_info.w_h_ratio) as VideoMag;
-                    }
-                    changed = true;
-                }
-                // Clamp range to make it somewhat sane
-                present.dim.x = (present.dim.x).clamp(1, 4096);
-                present.dim.y = (present.dim.y).clamp(1, 4096);
-                if changed
-                    && !present
-                        .texture
-                        .create((present.dim.x).into(), (present.dim.y).into())
-                {
-                    panic!("Failed to create texture");
-                }
-                if let Some(mut speed) = mpv.get_property::<Speed>() {
-                    ui.label("Playback speed");
-                    if ui.add(egui::Slider::new(&mut speed, 0.1..=2.0)).changed() {
-                        mpv.set_property::<Speed>(speed);
-                    }
-                }
-                if let Some(mut vol) = mpv.get_property::<Volume>() {
-                    ui.label("Playback volume");
-                    if ui.add(egui::Slider::new(&mut vol, 0.0..=150.0)).changed() {
-                        mpv.set_property::<Volume>(vol);
-                    }
-                }
-            });
+            bottom_bar_ui(ui, src_info, present, mpv, video_area_max_dim);
         });
         video_area_max_dim.y = re.response.rect.top() as VideoMag;
         let re = egui::SidePanel::right("right_panel").show(ctx, |ui| {
@@ -129,6 +66,79 @@ pub(crate) fn ui(
         });
         video_area_max_dim.x = re.response.rect.left() as VideoMag;
     }
+}
+
+fn bottom_bar_ui(
+    ui: &mut egui::Ui,
+    src_info: &source::Info,
+    present: &mut Present,
+    mpv: &mut Mpv,
+    video_area_max_dim: &mut VideoDim<coords::Present>,
+) {
+    ui.horizontal(|ui| {
+        ui.label(format!(
+            "{}/{}",
+            FfmpegTimeFmt(src_info.time_pos),
+            FfmpegTimeFmt(src_info.duration)
+        ));
+        ui.style_mut().spacing.slider_width = ui.available_width();
+        let mut pos = src_info.time_pos;
+        if ui
+            .add(egui::Slider::new(&mut pos, 0.0..=src_info.duration).show_value(false))
+            .changed()
+        {
+            mpv.set_property::<TimePos>(pos);
+        }
+    });
+    ui.horizontal(|ui| {
+        let mut changed = false;
+        ui.label("Video width");
+        if ui.add(egui::DragValue::new(&mut present.dim.x)).changed() {
+            present.dim.y = (present.dim.x as f64 / src_info.w_h_ratio) as VideoMag;
+            changed = true;
+        }
+        ui.label("Video height");
+        if ui.add(egui::DragValue::new(&mut present.dim.y)).changed() {
+            present.dim.x = (present.dim.y as f64 * src_info.w_h_ratio) as VideoMag;
+            changed = true;
+        }
+        if ui.button("orig").clicked() {
+            present.dim.x = src_info.dim.x as VideoMag;
+            present.dim.y = src_info.dim.y as VideoMag;
+            changed = true;
+        }
+        if ui.button("fit").clicked() {
+            present.dim.y = video_area_max_dim.y;
+            present.dim.x = (present.dim.y as f64 * src_info.w_h_ratio) as VideoMag;
+            if present.dim.x > video_area_max_dim.x {
+                present.dim.x = video_area_max_dim.x;
+                present.dim.y = (present.dim.x as f64 / src_info.w_h_ratio) as VideoMag;
+            }
+            changed = true;
+        }
+        // Clamp range to make it somewhat sane
+        present.dim.x = (present.dim.x).clamp(1, 4096);
+        present.dim.y = (present.dim.y).clamp(1, 4096);
+        if changed
+            && !present
+                .texture
+                .create((present.dim.x).into(), (present.dim.y).into())
+        {
+            panic!("Failed to create texture");
+        }
+        if let Some(mut speed) = mpv.get_property::<Speed>() {
+            ui.label("Playback speed");
+            if ui.add(egui::Slider::new(&mut speed, 0.1..=2.0)).changed() {
+                mpv.set_property::<Speed>(speed);
+            }
+        }
+        if let Some(mut vol) = mpv.get_property::<Volume>() {
+            ui.label("Playback volume");
+            if ui.add(egui::Slider::new(&mut vol, 0.0..=150.0)).changed() {
+                mpv.set_property::<Volume>(vol);
+            }
+        }
+    });
 }
 
 fn timespans_ui(ui: &mut egui::Ui) {
