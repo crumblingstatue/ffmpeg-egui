48d2e7ef0afeb7f04d9b379bfa26aeacdeb1c1b0
crumblingstatue <radiantstatue@gmail.com>
2024-05-29 12:35:01 +0200

    Dep update

--- src/main.rs
+++ src/main.rs
@@ -242,7 +242,7 @@ fn main() {
             interact_state.pan_pos.y = orig_img.y - diff_y;
         }
         sf_egui
-            .do_frame(|ctx| {
+            .do_frame(&mut rw, |ctx| {
                 ui::ui(
                     ctx,
                     &mpv,
